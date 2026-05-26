ec7b0cd8c82907b78ac26c0812b3ea25ad13ae62
Ahmed Al-Tuwaijari <altuwaijari.ahmed@gmail.com>
2026-05-26 19:28:23 +0300

    fix: add /authorize alias for /oauth/authorize

--- server.js
+++ server.js
@@ -70,7 +70,7 @@ app.get("/oauth/authorize", (req, res) => {
 
   const params = new URLSearchParams({
     client_id: GITHUB_CLIENT_ID,
-    redirect_uri: `${BASE_URL}/oauth/callback`,
+    redirect_uri: req.query.redirect_uri,
     scope: "read:user repo",
     state: internalState,
   });
